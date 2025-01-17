50de5ac1217ac43957b6451e0be32e77b8c7e9ce
jalaa farhat <jalaa.c.m@gmail.com>
2025-01-17 12:28:03 +0200

    installing express-async-handler , added Error handling middlewares , updating the server and user Controller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,45 +1,43 @@
-const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+
 //Registration
-const register = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    //validate
-    if (!username || !email || !password) {
-      res.status(400);
-      throw new Error("All fields are required!");
-    }
-    //check if email is taken
-    const userExist = await User.findOne({ email });
-    if (userExist) {
-      res.status(400);
-      throw new Error("User already exists!");
-    }
-    //hash the user password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    //create the user
-    const newUser = new User({ username, email, password: hashedPassword });
-    //add the date the trial will end
-    newUser.trialExpires = new Date(
-      new Date().getTime() + newUser.trialPeriod * 24 * 60 * 60 * 1000
-    );
-    //save the user
-    await newUser.save();
-
-    res.json({
-      status: true,
-      message: "Registration was successful",
-      user: {
-        username,
-        email,
-      },
-    });
-  } catch (error) {
-    throw new Error(error);
+const register = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  //validate
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required!");
   }
-};
+  //check if email is taken
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists!");
+  }
+  //hash the user password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //create the user
+  const newUser = new User({ username, email, password: hashedPassword });
+  //add the date the trial will end
+  newUser.trialExpires = new Date(
+    new Date().getTime() + newUser.trialPeriod * 24 * 60 * 60 * 1000
+  );
+  //save the user
+  await newUser.save();
+
+  res.json({
+    status: true,
+    message: "Registration was successful",
+    user: {
+      username,
+      email,
+    },
+  });
+});
 
 //Login
 
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 const usersRouter = require("./routes/usersRouter");
+const { errorHandler } = require("./middlewares/errorMiddlewear");
 require("./utils/connectDB")(); //same as a=require("./utils/connectDB") and calling a()
+
 const app = express();
 const PORT = process.env.PORT || 8090;
 
@@ -9,6 +12,10 @@ app.use(express.json());
 
 //Route
 app.use("/api/v1/users", usersRouter);
+
+//error handler middlewear
+app.use(errorHandler);
+
 //start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
